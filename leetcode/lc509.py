e50ccd5b8ad90d5fa25bddca2a867609fbf8ce80
Jianbo Dai <jianbodai819@gmail.com>
2020-04-04 02:26:42 +0100

    add python folder

---
+++ leetcode/lc509.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def fib1(self, N: int) -> int:
+        return 2
+
+    def fib2(self, N: int) -> int:
+        return 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.fib1(30))
