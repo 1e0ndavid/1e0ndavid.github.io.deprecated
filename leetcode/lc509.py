24ce591fdb07f08b27bc380a8731e0d49fb5330f
Jianbo Dai <jianbodai819@gmail.com>
2020-04-09 15:49:31 +0100

    add 1st algorithm

--- leetcode/lc509.py
+++ leetcode/lc509.py
@@ -1,12 +1,35 @@
 
 class Solution:
     def fib1(self, N: int) -> int:
-        return 2
+        res = [0, 1]
+        if N <= 1:
+            return res[N]
+        else:
+            return self.fib1(N-1) + self.fib1(N-2)
 
     def fib2(self, N: int) -> int:
-        return 1
+        res = [0, 1]
+        if N <= 1:
+            return res[N]
+        else:
+            fibNminusOne = 0
+            fibNminusTwo = 1
+            for _ in range(N):
+                fibN = fibNminusOne + fibNminusTwo
+                fibNminusTwo, fibNminusOne = fibNminusOne, fibN
+            return fibN
+
+    def fib3(self, N: int) -> int:
+        fibNminusOne, fibNminusTwo = 0, 1
+        for _ in range(N):
+            fibNminusTwo, fibNminusOne = fibNminusOne, fibNminusOne + fibNminusTwo
+        return fibNminusOne
+
+    def fib4(self, N: int) -> int:
+        return 0
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.fib1(30))
+    for i in range(11):
+        print(s.fib1(i), s.fib2(i), s.fib3(i))
