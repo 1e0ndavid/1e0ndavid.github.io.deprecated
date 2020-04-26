0514e631198376665a16d1e1dae15dc0520d335f
Jianbo Dai <jianbodai819@gmail.com>
2020-04-26 02:34:36 +0100

    merge two ordered lists

---
+++ leetcode/lc088.py
@@ -0,0 +1,9 @@
+class Solution():
+    def mergeLists1(self, nums1, m, nums2, n):
+        # print(nums1, m, nums2, n)
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.mergeLists1([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
